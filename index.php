c8593ae218c77c7fa9faedacd0727e29d7ff1306
Shubham Kapoor <shubham28204@gmail.com>
2022-06-11 19:37:26 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,306 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="script/main.js"></script>
+    <script src="https://kit.fontawesome.com/c682dc3811.js" crossorigin="anonymous"></script>
+     <link rel="stylesheet" href="css/style.css">
+    <title>Shubham Kapoor</title>
+
+</head>
+<body>
+    <!-- NAV -->
+    <nav class="navbar fixed-top navbar-expand-lg bg-primary navbar-light fw-bold " id="navbar">
+        <div class="container">
+            <a href="#" class="navbar-brand fw-bold"> <u>Portfolio</u> </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu" >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+           
+            <div class="collapse navbar-collapse" id="navmenu">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-items">
+                        <a href="#home" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-items">
+                        <a href="#skills" class="nav-link">Services</a>
+                    </li>
+                    <li class="nav-items">
+                        <a href="#project" class="nav-link">Project</a>
+                    </li>
+                    <li class="nav-items">
+                        <a href="#contact" class="nav-link">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+<!-- NAV END -->
+<!-- Intro -->
+<section class="bg-white text-primary p-5 text-start text-sm-center" id="home">
+<div class="container">
+<div class="d-sm-flex justify-content-between align-items-center">
+    <div class="text-center text-md-start">
+        <h5><span class="text-dark " id="text-zero">Hello,</span></h5>
+        <h1><span class="text-dark fw-bold" id="text-one">I'm Shubham Kapoor</span></h1>
+        <h4><span class="text-primary"id="text-two">Full Stack Web Developer</span></h4>
+        <h6><span class="text-dark"id="text-three">From India</span></h3>
+        
+            <a href="https://drive.google.com/file/d/1VZHFFwWj6aPhPe5cctRd78T5nZ49B_V-/view?usp=sharing" target="_blank"><button class="btn btn-primary btn-md">Download CV</button></a>
+    </div>
+   
+        <img class="img-fluid w-50 align-right py-5" id="first_img"src="images/CoffeeDoddle_blue.svg" alt="">
+        
+   
+</div>
+</div>
+</div>
+</section>
+<!-- Intro End -->
+<img src="images/wave.svg" alt="">
+<!-- Quote -->
+<section class="bg-primary text-white text-center p-3">
+    <div class="container">
+        <div class="d-flex justify-content-center align-items-center">
+            <img class="quote pb-5 mb-5 " src="images/quote.png" alt="">
+            <div>
+                <br><br><br>
+                <p class="fw-bold display-3" >Caffeine maintains my wavy personality.</p>
+            </div>
+            
+        </div>
+    </div>
+</section>
+<!-- Blank -->
+
+<section class="bg-primary ">
+<br><br><br>
+</section >
+<img src="images/wave_d.svg" alt="">
+<!-- Skill -->
+<section class="bg-white text-primary text-start p-0" >
+              <div class="container" id="skills" >
+                        <p class="text-dark fw-bold text-center display-4"  > My Skills </p>
+                <div class="d-sm-flex justify-content-between p-5">    
+                     
+                                            <p class="skill-text" id="text-three">
+                                        <span class="fw-bold">
+                                            Skills Reflects Our Knowledge
+                                            </span>  <br>
+                                            Being a Full Stack Developer it includes using a range of different technologies and languages (such as Java, JavaScript, HTML, PHP ) to develop applications.To gain Full Stack Developers approach software holistically since to cater to both user experience and functionality.</p>
+       
+                    
+                    <div>
+                        <table class="fw-bold fw-size fs-5"> 
+                            <tr>
+                                <td >HTML</td>
+                                <td class="px-5">
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>CSS</td>
+                                <td class="px-5">
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star-half"></i>
+                                
+                                
+                            </td>
+                            </tr>
+                            <tr>
+                                <td>Bootstrap</td>
+                                <td class="px-5">
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star-half"></i>
+                                
+                            </td>
+                            </tr>
+                            <tr>
+                                <td >JS</td>
+                                <td class="px-5">
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                
+                                
+                            </td>
+                            </tr>
+                            <tr>
+                                <td>PHP</td>
+                                <td class="px-5">
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star-half"></i>
+                               
+                            </td>
+                            </tr>
+                        </table>
+                </div>
+                </div>
+            
+            </section>
+            <!-- Skill End -->
+            <hr class="style-seven">
+            <!-- Services -->
+            <section>
+<div class="container">
+    <p class="text-dark fw-bold text-center display-4" id="project" > Services</p>
+
+        <div class="row">
+            <div class="col m-3 h-2 d-flex flex-column " id="serv-box">
+                <div class="text-center p-2">
+                    <i class="fa-solid fa-laptop-code"></i>
+                </div>
+                <div class="text-center p-2">
+                    <p class=" fw-bold text-center fs-3"> Web Development</p>
+                </div>
+            </div>
+        
+            <div class="col m-3 h-2 d-flex flex-column "id="serv-box">
+                <div class="text-center p-2">
+                    <i class="fa-brands fa-500px"></i>
+                </div>
+                <div class="text-center p-2">
+                    <p class=" fw-bold text-center fs-3"> Database Management</p>
+                </div>
+            </div>
+    
+        </div>
+        <div class="row my-3">
+            <div class="col m-3 h-2 d-flex flex-column "id="serv-box">
+                <div class="text-center p-2">
+                    <i class="fa-brands fa-500px"></i>
+                </div>
+                <div class="text-center p-2">
+                    <p class=" fw-bold text-center fs-3"> UI / UX</p>
+                </div>
+            </div>
+    
+            <div class="col m-3 h-2 d-flex flex-column "id="serv-box">
+                <div class="text-center p-2">
+                    <i class="fa-brands fa-digital-ocean"></i>
+                </div>
+                <div class="text-center p-2">
+                    <p class="fw-bold text-center fs-3"> Graphic Illustration</p>
+                </div>
+            </div>  
+    
+        </div>
+
+</div>
+<!-- Services End -->
+<hr class="style-seven">
+
+            <section>
+<!-- Project  -->
+<div class="container" >
+    <p class="text-dark fw-bold text-center display-4"> Projects</p>
+<!-- accordian -->
+<div class="accordion accordion-flush fs-5" id="accordianflush">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingOne">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        <h3 class="text-primary fw-bold">Official Website Of Software Development Cell</h1> 
+      </button>
+    </h2>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordianflush">
+      <div class="accordion-body">
+    </div>Link : <a href="https://iitmjanakpuri-sdc.in/" target="_blank">Software Development Cell Of IITM Janakpuri </a> </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+      <h3 class="text-primary fw-bold">Sign In Page</h1> 
+      </button>
+    </h2>
+    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordianflush">
+      <div class="accordion-body">
+      </div>Link : <a href="../portfolio/signin/index.php" target="_blank">Sign In Page </a> </div>
+      </div>
+    </div>
+  </div>
+  
+</div>
+
+
+</div>
+    <img src="images/wave_b.svg" alt=""  >
+
+</section>
+<!-- SERVICE END -->
+<!-- Contact -->
+<section >
+    
+        <div class="bg-dark text-white" id="contact" >
+        
+            <div class="container-lg">
+                <p class="text-white fw-bold text-center display-4">Contact</p>
+                <div class="row">
+                    <div class="col-md-5  text-white py-4 ">
+                        <div class="contact-item d-flex mb-3">
+
+                            <div class="icon fs-5 text-primary">
+                                <i class="fas fa-envelope"></i>  
+                            </div>
+                            <div class="text ms-3 ">
+                                <p class="fs-5 fw-bold">Email</p>
+                                <p class="text-muted ">shubham28204@gmail.com</p>
+                                
+                            </div>
+                        </div>
+                        <div class="contact-item d-flex mb-3">
+
+                            <div class="icon fs-5 text-primary">
+                                <i class="fas fa-phone"></i>  
+                            </div>
+                            <div class="text ms-3 ">
+                                <p class="fs-5 fw-bold">Phone </p>
+                                <p class="text-muted ">+91 9540028204</p>
+                                
+                            </div>
+                        </div>
+                        <div class="contact-item d-flex mb-3">
+
+                            <div class="icon fs-5 text-primary">
+                                <i class="fas fa-map-marker-alt"></i>  
+                            </div>
+                            <div class="text ms-3 ">
+                                <p class="fs-5 fw-bold">Address</p>
+                                <p class="text-muted ">Delhi, India</p>
+                                
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="col-md-7">
+                        <div class="cont-img">
+
+                            <img class="img-fluid d-block w-40" src="images/sit.png" alt="">
+                        </div>
+
+                    </div>
+                </div>
+                </div>
+            </div>
+    </div>
+    <!-- FOOTER -->
+<footer class="bg-primary text-white text-center fw-bold p-3 ">
+    Made with love <span class="fw-bolder"> © 2022 All Rights Reserved </span>
+</footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,11 @@
+## Intial Portflio Project
+
+I made this while learning Bootstrap 5
+
+## Things to change
+
+-Basically Everything
+-New UI
+-Update Skills
+-Add Animation
+-Use Mario or Cofee Illus
